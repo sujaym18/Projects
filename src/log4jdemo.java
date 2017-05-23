ccdfd9cedd760136e7a5c0ca09b4e6786251608d
Sujay.Mukadam <Sujay.Mukadam@EEEPC0282.extentia05.local>
2017-05-23 12:44:37 +0530

    First commit

---
+++ src/log4jdemo.java
@@ -0,0 +1,19 @@
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class log4jdemo {
+	//static final 
+	Logger log=Logger.getLogger("sujay");
+@Test
+//public static void main(String args[]){
+public void test1(){
+	WebDriver driver = new FirefoxDriver();
+	driver.get("http://www.google.com");
+	driver.manage().window().maximize();
+	log.debug("Site opens");
+	driver.close();
+	log.debug("site close");
+	}
+}
